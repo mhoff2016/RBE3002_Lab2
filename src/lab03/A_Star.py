31f3dcfaac047c7e4fa2f3c25ef07fc6750f57ae
David Abraham <dabraham2@ak120d-05.wpi.edu>
2019-04-03 15:49:14 -0400

    added A* algorithm and started with server client

--- src/lab03/A_Star.py
+++ src/lab03/A_Star.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+import sys
 
 
 class A_Star:
@@ -13,7 +14,7 @@ class A_Star:
         """
 
         rospy.init_node("a_star")  # start node
- 
+
 
     def handle_a_star(self, req):
 
@@ -23,9 +24,17 @@ class A_Star:
             :param req: GetPlan
             :return: Path()
         """
-        pass
+        print ("Returning path...")
+        return a_star(req.start, req.goal)
 
-       
+
+    def a_star_server(self):
+        rospy.init_node('a_star_server')
+        s =  rospy.Service('a_star', GetPlan, handle_a_star)
+        print "ready to star that A"
+        rospy.spin()
+
+
 
     def dynamic_map_client(self):
 
@@ -45,9 +54,31 @@ class A_Star:
             :param goal: tuple of goal pose
             :return: dict of tuples
         """
-        pass
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from = {}
+        cost_so_far = {}
+        came_from[start] = None
+        cost_so_far[start] = 0
 
-      
+        while not frontier.empty():
+            current = frontier.get()
+
+            if current == goal:
+                break
+
+            for next in self.neighbors(current):
+                new_cost = cost_so_far[current] + self.cost(current, next)
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost + heuristic(goal, next)
+                    frontier.put(next, priority)
+                    came_from[next] = current
+
+        return came_from, cost_so_far
+
+
+
     def euclidean_heuristic(self, point1, point2):
         """
             calculate the dist between two points
@@ -55,6 +86,15 @@ class A_Star:
             :param point2: tuple of location
             :return: dist between two points
         """
+        x1 = point1[0]
+        y1 = point1[1]
+
+        x2 = point2[0]
+        y2 = point2[1]
+
+        out =  sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+
     pass
 
     def move_cost(self, current, next):
@@ -74,9 +114,9 @@ class A_Star:
             :param goal: starting value
             :param came_from: dictionary of tuples
             :return: list of tuples
-       """
-       pass
-  
+        """
+        pass
+
 
     def optimize_path(self, path):
         """
@@ -107,4 +147,5 @@ class A_Star:
 
 
 if __name__ == '__main__':
-    pass
+    print "runnnnnnnning"
+    star = A_Star()
